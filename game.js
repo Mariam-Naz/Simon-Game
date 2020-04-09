26949510a61b90dd20553ae4edb56afe542a3336
unknown <mariamnaz212@gmail.com>
2020-04-09 21:12:31 +0500

    complete and correct logic of the game

--- game.js
+++ game.js
@@ -4,13 +4,23 @@ var buttonColors=["red","blue","green","yellow"];
 var gamePattern= [];
 var level=0;
 var start=true;
+var count=0;
+var countClick=0
+
+$('body').on('keydown',function(){
+    if(start==true)
+        nextSequence();
+    start=false;
+});
 
 $('.btn').on('click', function(){
     var userChosenColour=this.id;
     userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
     playSound(userChosenColour);
     console.log(userChosenColour);
     animatePress(userChosenColour);
+    countClick++
     checkAnswer(level);
 });  
 
@@ -18,10 +28,35 @@ function nextSequence(){
     var randomNumber= Math.floor(Math.random()*4);
     var randomChosenColour=buttonColors[randomNumber];
     gamePattern.push(randomChosenColour);
+    console.log(gamePattern);
     $('#'+ randomChosenColour).fadeOut(100).fadeIn(100).fadeIn(100);
     playSound(randomChosenColour);
-    $('h1').text('Level ' + level);
     level++;
+    $('h1').text('Level ' + level);
+
+}
+var i=0;
+function checkAnswer(currentLevel){
+    while(i<userClickedPattern.length){
+        if(gamePattern[i]==userClickedPattern[i]){
+            count++; 
+        }
+        else if(gamePattern[i]!=userClickedPattern[i]){
+            $('h1').text('you lost');
+        }
+        i++;
+    }
+    console.log(count);
+console.log(currentLevel);
+if(count==currentLevel){
+    userClickedPattern=[];
+    countClick=0;
+    count=0;
+    i=0;
+    nextSequence();
+    
+}
+
 }
 
 function playSound(name){
@@ -36,19 +71,3 @@ function animatePress(currentColour){
    });
   
 }
-
-function checkAnswer(currentLevel){
-    for(i=0 ;i<=currentLevel;i++){
-        if(gamePattern[i]==userClickedPattern[i])
-            nextSequence();
-    }
-
-}
-$('body').on('keydown',function(){
-    if(start==true)
-        nextSequence();
-    start=false;
-    
-  
-});
-
